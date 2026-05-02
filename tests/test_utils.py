389d06f2a5b9281bae133e53e4af15d54bb7a529
Eugen <trushin00332211@gmail.com>
2026-05-02 21:41:29 +0500

    Тест test_filter_aeroplanes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,3 +9,10 @@ class TestUtils(unittest.TestCase):
         self.p2 = Aeroplane("A2", "UK", 200.0, 10000.0)
         self.p3 = Aeroplane("A3", "USA", 150.0, 8000.0)
         self.planes = [self.p1, self.p2, self.p3]
+
+    def test_filter_aeroplanes(self):
+        """Тест фильтрации по стране регистрации."""
+        filtered = filter_aeroplanes(self.planes, ["USA"])
+        self.assertEqual(len(filtered), 2)
+        self.assertTrue(all(p.origin_country == "USA" for p in filtered))
+
